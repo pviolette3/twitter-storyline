0e8d4cb7e748cf1e7eb785e7fed81dfccf27a0ea
Patrick Violette <violette.patrick718@gmail.com>
2015-08-22 23:31:47 -0400

    Merged

--- download.py
+++ download.py
@@ -3,20 +3,6 @@ import json
 import requests
 from tweet import *
 
-def main():
-    with open('credentials.json') as f:
-        credentials = json.load(f)
-    twitter = twython.Twython(
-        credentials['APP_KEY'],
-        credentials['APP_SECRET'],
-        credentials['ACCESS_TOKEN'],
-        credentials['ACCESS_TOKEN_SECRET'])
-    twitter.verify_credentials()
-
-if __name__ == '__main__':
-    import twython
-    import json
-
 class TweetSampler(twython.TwythonStreamer):
     def on_success(self, data):
         if 'text' in data and 'id_str' in data and 'user' in data and 'id_str' in data['user'] and 'timestamp_ms' in data and 'place' in data and 'id' in data['place']:
@@ -25,7 +11,6 @@ class TweetSampler(twython.TwythonStreamer):
             saveNewTweets([t])
             print "Yay"
             # print json.dumps(data)
-
 
     def on_error(self, status_code, data):
         print status_code
