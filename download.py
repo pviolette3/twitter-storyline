b8549149dd9e75575b552af61003174aaaa0b78e
Philippe Laban <plaban3@gatech.edu>
2015-08-22 23:30:48 -0400

    Yolo

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 import twython
 import json
 import requests
+from tweet import *
 
 def main():
     with open('credentials.json') as f:
@@ -13,13 +14,18 @@ def main():
     twitter.verify_credentials()
 
 if __name__ == '__main__':
-import twython
-import json
+    import twython
+    import json
 
 class TweetSampler(twython.TwythonStreamer):
     def on_success(self, data):
-        if 'text' in data:
-            print json.dumps(data)
+        if 'text' in data and 'id_str' in data and 'user' in data and 'id_str' in data['user'] and 'timestamp_ms' in data and 'place' in data and 'id' in data['place']:
+            time = int(float(data['timestamp_ms'])/1000.0)
+            t = Tweet(data['id_str'], data['text'], data['user']['id_str'], time, data['place']['id'])
+            saveNewTweets([t])
+            print "Yay"
+            # print json.dumps(data)
+
 
     def on_error(self, status_code, data):
         print status_code
--- tweet.py
+++ tweet.py
@@ -2,32 +2,22 @@ from collections import namedtuple
 import json
 from dbco import * # this imports the db connexion
 
-Tweet = namedtuple('Tweet', ['guid', 'text', 'author', 'timestamp'])
-class Tweet(namedtuple('Tweet', ['guid', 'text', 'author', 'timestamp'])):
-    def __new__(cls, guid='', text='', author='', timestamp=0):
-        return super(Tweet, cls).__new__(cls, guid, text, author, timestamp)
+Tweet = namedtuple('Tweet', ['guid', 'text', 'author', 'timestamp', 'place'])
+class Tweet(namedtuple('Tweet', ['guid', 'text', 'author', 'timestamp', 'place'])):
+    def __new__(cls, guid='', text='', author='', timestamp=0, place=''):
+        return super(Tweet, cls).__new__(cls, guid, text, author, timestamp, place)
 
 def saveNewTweets(newTweets):
 	Ts = []
 	for t in newTweets:
 		if isValid(t):
 			Ts.append(t._asdict())
-		else:
-			print "Article from source: ", a.source, "feed: ", a.feed, " was invalid"
 	if len(Ts) > 0:
 		insertArticles(Ts)
 
 def insertArticles(Ts):
 	for t in Ts:
 		db.tweet.update({'guid': t['guid']}, {'$set': t}, upsert=True) # if the GUID is already in the set
-
-def saveNewArticlesFile(newArticles):
-	with open("DB_ex.txt", "a") as f:
-		for a in newArticles:
-			if isValid(a):
-				f.write(json.dumps(a._asdict())+'\n')
-			else:
-				print "Article from source: ", a.source, "feed: ", a.feed, " was invalid"
 
 def isValid(t):
 	if t.guid == '':
@@ -40,7 +30,5 @@ def isValid(t):
 		return False
 	return True
 
-
-tw = Tweet('blala', 'hello my tweet', 'phili', 234567567)
-
-saveNewTweets([tw])
+# tw = Tweet('blala', 'hello my tweet', 'phili', 234567567)
+# saveNewTweets([tw])
