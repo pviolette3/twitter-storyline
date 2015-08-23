db442acf8e5a7cb34ff47a704fa9c6cda04f9c65
Patrick Violette <violette.patrick718@gmail.com>
2015-08-22 22:56:11 -0400

    Stream from sample

--- download.py
+++ download.py
@@ -18,15 +18,23 @@ if __name__ == '__main__':
 import twython
 import json
 
+class TweetSampler(twython.TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            print json.dumps(data)
+
+    def on_error(self, status_code, data):
+        print status_code
+
 def main():
     with open('credentials.json') as f:
         credentials = json.load(f)
-    twitter = twython.Twython(
+    stream = TweetSampler(
         credentials['APP_KEY'],
         credentials['APP_SECRET'],
         credentials['ACCESS_TOKEN'],
         credentials['ACCESS_TOKEN_SECRET'])
-    twitter.verify_credentials()
+    stream.statuses.sample()
 
 if __name__ == '__main__':
 >>>>>>> f317f2a8c70e5926fa910b4c7b114f4019e00847
