03d5b223a685d1af2a375cb4c4abb09ba08705d0
Patrick Violette <violette.patrick718@gmail.com>
2015-08-22 23:02:21 -0400

    Stream from ATL .. ?

--- download.py
+++ download.py
@@ -26,6 +26,34 @@ class TweetSampler(twython.TwythonStreamer):
     def on_error(self, status_code, data):
         print status_code
 
+ATL_BOUNDING_BOX = {
+    "coordinates": [
+    [
+      [
+        -85.605166,
+        30.355644
+      ],
+      [
+        -85.605166,
+        35.000771
+      ],
+      [
+        -80.742567,
+        35.000771
+      ],
+      [
+        -80.742567,
+        30.355644
+      ],
+      [
+        -85.605166,
+        30.355644
+      ]
+    ]
+]}
+
+ATL_BOUNDING_BOX_CORNERS = '-85.605166,30.355644,-80.742567,35.000771'
+
 def main():
     with open('credentials.json') as f:
         credentials = json.load(f)
@@ -34,7 +62,7 @@ def main():
         credentials['APP_SECRET'],
         credentials['ACCESS_TOKEN'],
         credentials['ACCESS_TOKEN_SECRET'])
-    stream.statuses.sample()
+    stream.statuses.filter(locations=ATL_BOUNDING_BOX_CORNERS)
 
 if __name__ == '__main__':
 >>>>>>> f317f2a8c70e5926fa910b4c7b114f4019e00847
