e8bdad3155edb00e9badc227ac93e63a1fbf7775
Patrick Violette <violette.patrick718@gmail.com>
2015-08-22 23:30:43 -0400

    Fixed bounding box

--- download.py
+++ download.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import twython
-import json
-import requests
-
-def main():
-    with open('credentials.json') as f:
-        credentials = json.load(f)
-    twitter = twython.Twython(
-        credentials['APP_KEY'],
-        credentials['APP_SECRET'],
-        credentials['ACCESS_TOKEN'],
-        credentials['ACCESS_TOKEN_SECRET'])
-    twitter.verify_credentials()
-
-if __name__ == '__main__':
-=======
 import twython
 import json
 
@@ -26,33 +9,7 @@ class TweetSampler(twython.TwythonStreamer):
     def on_error(self, status_code, data):
         print status_code
 
-ATL_BOUNDING_BOX = {
-    "coordinates": [
-    [
-      [
-        -85.605166,
-        30.355644
-      ],
-      [
-        -85.605166,
-        35.000771
-      ],
-      [
-        -80.742567,
-        35.000771
-      ],
-      [
-        -80.742567,
-        30.355644
-      ],
-      [
-        -85.605166,
-        30.355644
-      ]
-    ]
-]}
-
-ATL_BOUNDING_BOX_CORNERS = '-85.605166,30.355644,-80.742567,35.000771'
+ATL_BOUNDING_BOX_CORNERS = '-84.7125826,33.325678,-83.3886814,33.9637709'
 
 def main():
     with open('credentials.json') as f:
@@ -65,5 +22,4 @@ def main():
     stream.statuses.filter(locations=ATL_BOUNDING_BOX_CORNERS)
 
 if __name__ == '__main__':
->>>>>>> f317f2a8c70e5926fa910b4c7b114f4019e00847
     main()
--- p.py
+++
@@ -1,4 +0,0 @@
-import json
-
-with open('credentials.json') as f:
-        credentials = json.load(f)
